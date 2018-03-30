4ad594bbf9eb0e9a27ba00706c85fd0943e5109d
Piotr Mińkowski <piotr_minkowski@wp.pl>
2018-03-31 00:06:20 +0200

    fixes

--- ignite-rest-service/src/main/java/pl/piomin/services/ignite/IgniteRestApplication.java
+++ ignite-rest-service/src/main/java/pl/piomin/services/ignite/IgniteRestApplication.java
@@ -34,7 +34,9 @@ public class IgniteRestApplication {
 		cfg.setIgniteInstanceName("ignite-cluster-node");
 		
 		CacheConfiguration<Long, Person> ccfg1 = new CacheConfiguration<>("PersonCache");
+		ccfg1.setIndexedTypes(Long.class, Person.class);
 		CacheConfiguration<Long, Contact> ccfg2 = new CacheConfiguration<>("ContactCache");
+		ccfg2.setIndexedTypes(Long.class, Contact.class);
 		cfg.setCacheConfiguration(ccfg1, ccfg2);
 		
 //		DataStorageConfiguration storageCfg = new DataStorageConfiguration();
