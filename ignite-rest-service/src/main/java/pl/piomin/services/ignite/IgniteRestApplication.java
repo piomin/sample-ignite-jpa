751a52ab50d6d6ab95bfbfb97518f07984d5af38
Piotr Mińkowski <piotr_minkowski@wp.pl>
2018-05-06 00:03:46 +0200

    adding cache configuration

--- ignite-rest-service/src/main/java/pl/piomin/services/ignite/IgniteRestApplication.java
+++ ignite-rest-service/src/main/java/pl/piomin/services/ignite/IgniteRestApplication.java
@@ -2,6 +2,7 @@ package pl.piomin.services.ignite;
 
 import org.apache.ignite.Ignite;
 import org.apache.ignite.Ignition;
+import org.apache.ignite.configuration.CacheConfiguration;
 import org.apache.ignite.configuration.IgniteConfiguration;
 import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi;
 import org.apache.ignite.spi.discovery.tcp.ipfinder.kubernetes.TcpDiscoveryKubernetesIpFinder;
@@ -9,6 +10,9 @@ import org.apache.ignite.springdata.repository.config.EnableIgniteRepositories;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import pl.piomin.services.ignite.model.Contact;
+import pl.piomin.services.ignite.model.Person;
 
 @SpringBootApplication
 @EnableIgniteRepositories
@@ -26,9 +30,15 @@ public class IgniteRestApplication {
 		spi.setIpFinder(ipFinder);
 		
 		IgniteConfiguration cfg = new IgniteConfiguration();
-//		cfg.setClientMode(true);
+		cfg.setClientMode(true);
 		cfg.setDiscoverySpi(spi);
-		cfg.setIgniteInstanceName("kubernetes-cluster");
+//		cfg.setIgniteInstanceName("kubernetes-cluster");
+		
+		CacheConfiguration<Long, Person> ccfg1 = new CacheConfiguration<>("PersonCache");
+		ccfg1.setIndexedTypes(Long.class, Person.class);
+		CacheConfiguration<Long, Contact> ccfg2 = new CacheConfiguration<>("ContactCache");
+		ccfg2.setIndexedTypes(Long.class, Contact.class);
+		cfg.setCacheConfiguration(ccfg1, ccfg2);
 		
 		return Ignition.start(cfg);
 	}
